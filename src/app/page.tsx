6808f76537f13497f3215a12ad6a8f120a8247d0
AuthentifiAI-Udi <udi@authentifi.ai>
2025-04-06 15:11:09 -0400

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello World from Next.js on Cloud Run 🚀</h1>
+    </main>
+  );
+}
